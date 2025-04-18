8bc614dc6b1563c5934c0fa053cf00183b88d195
lgolikova <milagoul@gmail.com>
2025-04-18 19:25:18 +0300

    remake guess number game

--- HW_1/index.js
+++ HW_1/index.js
@@ -1,15 +1,27 @@
 function guessNumber() {
     let number = Math.floor(Math.random() * 100) + 1;
-    let answer = +prompt("Угадайте число от 1 до 100");
+    let answer = prompt("Угадайте число от 1 до 100");
     let counter = 1;
-    while(answer!==number) {
-        if (answer > number) {
-            alert(`Загаданное число меньше ${answer}. Попробуйте снова.`);
-        } else if (answer < number) {
-            alert(`Загаданное число больше ${answer}. Попробуйте снова.`);
-        }
-        counter++;
-        answer = +prompt('Введите число');
+
+    if (answer === null) {
+        return;
     }
-    alert(`Вы угадали! Количество попыток: ${counter}`);
+
+    while(answer!==null) {
+        if (+answer > number) {
+            alert(`Загаданное число меньше ${answer}. Попробуйте снова.`);
+        } else if (+answer < number) {
+            alert(`Загаданное число больше ${answer}. Попробуйте снова.`);
+        } else if (+answer == number) {
+            alert(`Вы угадали! Количество попыток: ${counter}`);
+            return;
+        }
+
+        counter++;
+        answer = prompt('Введите число');
+    }
+
+    alert("Игра отменена.");
 }
+
+
