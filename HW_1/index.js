11bf1464ad9db996a8c361912fab72f6f682ae4d
lgolikova <milagoul@gmail.com>
2025-04-27 15:43:51 +0300

    made game-3 and game-4

--- HW_1/index.js
+++ HW_1/index.js
@@ -58,6 +58,93 @@ function mathGame() {
     } else {
         alert(`Неверно. Правильный ответ: ${solution}`);
     }
-
 }
 
+
+function getReversedText() {
+    let text = prompt('Введите текст');
+
+    if (text === null) {
+        return;
+    }
+
+    let reversedText = text.split('').reverse().join('');
+
+    alert(`Перевернутый текст: ${reversedText}`);
+}
+
+
+function playQuiz() {
+    const quiz = [
+        {
+            question: "Сколько примитивных типов данных в js?",
+            options: ["1. Семь", "2. Восемь", "3. Девять"],
+            correctAnswer: 1
+        },
+        {
+            question: "Какой результат у выражения Boolean(' ')",
+            options: ["1. true", "2. false", "3. undefined"],
+            correctAnswer: 1
+        },
+        {
+            question: "Что вернет выражение typeof null",
+            options: ["1. null", "2. object", "3. undefined"],
+            correctAnswer: 2
+        },
+        {
+            question: "Какой тип данных у переменной, которой ещё не присвоено значение?",
+            options: ["1. null", "2. false", "3. undefined"],
+            correctAnswer: 3
+        },
+        {
+            question: "Что вернёт выражение null == undefined",
+            options: ["1. true", "2. false", "3. Ошибка"],
+            correctAnswer: 1
+        },
+        {
+            question: "Как называется функция внутри объекта?",
+            options: ["1. Свойство", "2. Метод", "3. Конструктор"],
+            correctAnswer: 2
+        },
+        {
+            question: "Что будет выведено: console.log(0.1 + 0.2 === 0.3)",
+            options: ["1. true", "2. false", "3. undefined"],
+            correctAnswer: 2
+        },
+        {
+            question: "Каким будет результат: console.log(1 && 2 && 0)",
+            options: ["1. true", "2. false", "3. 0"],
+            correctAnswer: 3
+        },
+        {
+            question: "Какой метод добавляет элемент в начало массива?",
+            options: ["1. shift()", "2. unshift()", "3. pop()"],
+            correctAnswer: 2
+        },
+        {
+            question: "Как обозначается оператор нулевого слияния?",
+            options: ["1. !!", "2. ?", "3. ??"],
+            correctAnswer: 3
+        }
+    ];
+
+    let counter = 0;
+
+    for(let i = 0; i < quiz.length; i++) {
+        let answer = prompt(`Вопрос №${i + 1} (из 10) \n${quiz[i].question} \n\nВарианты ответов:\n${quiz[i].options.join('\n')}`);
+
+        if (answer === null) {
+            return;
+        }
+
+        if (+answer === quiz[i].correctAnswer || answer.toLowerCase().trim() === quiz[i].options[quiz[i].correctAnswer - 1].toLowerCase().trim().slice(3)) {
+            alert('Верно!');
+            counter++;
+        } else {
+            alert('Неверно');
+        }
+    }
+
+    alert(`Игра окончена. Количество правильных ответов: ${counter}`);
+
+}
